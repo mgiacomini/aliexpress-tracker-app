c12cf1b44bf7ff4e58afd7d2edd9dbd220842212
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-13 19:11:41 -0200

    permit aliexpress and wordpress sub params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.permit(:aliexpress_number, :wordpress_reference, :wordpress, :aliexpress).as_json
+    params.permit(:aliexpress_number, :wordpress_reference, wordpress: [], aliexpress: []).as_json
   end
 
 end
