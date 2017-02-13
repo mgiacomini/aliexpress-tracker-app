18d561381437bfce73e683de318804254e360202
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-13 18:53:10 -0200

    fixing namespace problems

--- app/workers/track_order_worker.rb
+++ app/workers/track_order_worker.rb
@@ -1,3 +1,8 @@
+require 'aliexpress/auth'
+require 'aliexpress/browser_builder'
+require 'aliexpress/tracker'
+require 'aliexpress/url_builder'
+
 class NullableLog
   def add_processed(message)
   end
@@ -16,12 +21,12 @@ class TrackOrderWorker
     params = order_params(params)
     aliexpress_number = params[:aliexpress_number]
     wordpress_reference = params[:wordpress_reference]
-    wordpress = Wordpress.new(params[:wordpress])
+    wordpress = ::Wordpress.new(params[:wordpress])
     aliexpress = ::AliexpressModel.new(params[:aliexpress])
-    order = Order.new(aliexpress: aliexpress, aliexpress_number: aliexpress_number, wordpress: wordpress, wordpress_reference: wordpress_reference)
+    order = ::Order.new(aliexpress: aliexpress, aliexpress_number: aliexpress_number, wordpress: wordpress, wordpress_reference: wordpress_reference)
 
-    browser = Aliexpress::BrowserBuilder.build
-    tracking_service = Aliexpress::Tracker.new(order, browser, NullableLog.new)
+    browser = ::Aliexpress::BrowserBuilder.build
+    tracking_service = ::Aliexpress::Tracker.new(order, browser, NullableLog.new)
     tracking_service.track!
     browser.close
   end
