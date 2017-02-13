ef0a02e299b2b1166174c2a65ec7ebe6a6722c6a
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-13 18:47:09 -0200

    returning json object to worker

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
 
   def track
-    TrackOrderWorker.perform_async(params)
+    TrackOrderWorker.perform_async(order_params)
     render nothing: true, status: :ok
+  end
+
+  private
+  def order_params
+    params.permit(:aliexpress_number, :wordpress_reference, :wordpress, :aliexpress).as_json
   end
 
 end
